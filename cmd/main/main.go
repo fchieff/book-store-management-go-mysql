152f0cf70a0142354f520be4ed4a284a5e0b9574
fchieff <lolfbl3@gmail.com>
2025-02-21 00:26:02 -0300

    0

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/fchieff/book-store-management-go-mysql.git/pkg/routes"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
